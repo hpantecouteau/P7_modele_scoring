97e32d7606f4434a7bd82ec4c388dbcd3a86d9c7
Hugo Pantecouteau <h.pantecouteau@beeguard.fr>
2022-09-29 17:32:08 +0200

    Set CLI to choose csv dataset

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import click
 import numpy as np
 import pandas as pd
 import gc
@@ -234,42 +235,50 @@ def credit_card_balance(num_rows = None, nan_as_category = True):
     return cc_agg
 
 
-def main(debug = False):
-    num_rows = 10000 if debug else None
-    df = application_train_test(num_rows)
-    with timer("Process bureau and bureau_balance"):
-        bureau = bureau_and_balance(num_rows)
-        print("Bureau df shape:", bureau.shape)
-        df = df.join(bureau, how='left', on='SK_ID_CURR')
-        del bureau
-        gc.collect()
-    with timer("Process previous_applications"):
-        prev = previous_applications(num_rows)
-        print("Previous applications df shape:", prev.shape)
-        df = df.join(prev, how='left', on='SK_ID_CURR')
-        del prev
-        gc.collect()
-    with timer("Process POS-CASH balance"):
-        pos = pos_cash(num_rows)
-        print("Pos-cash balance df shape:", pos.shape)
-        df = df.join(pos, how='left', on='SK_ID_CURR')
-        del pos
-        gc.collect()
-    with timer("Process installments payments"):
-        ins = installments_payments(num_rows)
-        print("Installments payments df shape:", ins.shape)
-        df = df.join(ins, how='left', on='SK_ID_CURR')
-        del ins
-        gc.collect()
-    with timer("Process credit card balance"):
-        cc = credit_card_balance(num_rows)
-        print("Credit card balance df shape:", cc.shape)
-        df = df.join(cc, how='left', on='SK_ID_CURR')
-        del cc
-        gc.collect()
-    print(f"Writing dataset in {OUTPUT_FILE}.")
-    df.to_csv(OUTPUT_FILE, index=False)
-    print("Done.")
+@click.command()
+@click.option('--source',
+    help='The source for data as CSV file',
+    required=False,
+    type=str
+)
+def main(debug = False, source: str = None):
+    if source is None:
+        num_rows = 10000 if debug else None
+        df = application_train_test(num_rows)
+        with timer("Process bureau and bureau_balance"):
+            bureau = bureau_and_balance(num_rows)
+            print("Bureau df shape:", bureau.shape)
+            df = df.join(bureau, how='left', on='SK_ID_CURR')
+            del bureau
+            gc.collect()
+        with timer("Process previous_applications"):
+            prev = previous_applications(num_rows)
+            print("Previous applications df shape:", prev.shape)
+            df = df.join(prev, how='left', on='SK_ID_CURR')
+            del prev
+            gc.collect()
+        with timer("Process POS-CASH balance"):
+            pos = pos_cash(num_rows)
+            print("Pos-cash balance df shape:", pos.shape)
+            df = df.join(pos, how='left', on='SK_ID_CURR')
+            del pos
+            gc.collect()
+        with timer("Process installments payments"):
+            ins = installments_payments(num_rows)
+            print("Installments payments df shape:", ins.shape)
+            df = df.join(ins, how='left', on='SK_ID_CURR')
+            del ins
+            gc.collect()
+        with timer("Process credit card balance"):
+            cc = credit_card_balance(num_rows)
+            print("Credit card balance df shape:", cc.shape)
+            df = df.join(cc, how='left', on='SK_ID_CURR')
+            del cc
+            gc.collect()
+        print(f"Writing dataset in {OUTPUT_FILE}.")
+        df.to_csv(OUTPUT_FILE, index=False)
+        print("Done.")
+    else:
 
 if __name__ == "__main__":
     with timer("Full model run"):
