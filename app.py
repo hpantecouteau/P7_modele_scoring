fa59eac3a252a52817ff8e51c903f9fd71244fa8
Hugo Pantecouteau <h.pantecouteau@beeguard.fr>
2022-11-24 13:20:20 +0100

    Afficher graph format png dans app

--- app.py
+++ app.py
@@ -45,13 +45,7 @@ app.layout = html.Div(children=[
     # dash_table.DataTable(id='display_data_df'),
     html.Div(id="display_data_df"),
 
-    html.Div(id="customer_data"),
-
-    html.Div(id="display_proba"),
-
-    html.Div(id="display_seuil"),
-
-    dcc.Graph(
+    html.Img(
         id='shap-graph'
     ),
 ])
@@ -87,34 +81,27 @@ def display_customer_data(n_clicks, customer_id):
     Input(component_id='submit-customer-id', component_property='n_clicks'),
     State(component_id='customer_id', component_property='value')
 )
-def display_customer_proba(n_clicks, customer_id):
+def display_interpetabilty_plot(n_clicks, customer_id):
     if n_clicks > 0:
-        r = requests.get(f'http://127.0.0.1:5000/api/customers/proba?id={customer_id}').json()
-        return f"Probabilité de remboursement : {r['P_OK']}"
+        r = requests.get(f'http://127.0.0.1:5000/api/customers/interpretability?id={customer_id}').json()
+        shap_values = pd.DataFrame(r).values[:,0]
+        print(shap_values)
+        print(shap_values.shape)
+        params = requests.get(f'http://127.0.0.1:5000/api/model/params').json()
+        explanation = shap.Explanation(values = shap_values, base_values=params["expected_value"], feature_names=params["features"])
+        with tempfile.TemporaryDirectory() as temp_dir:
+            plt.figure()
+            shap.plots.waterfall(explanation, show=False)
+            # plt.gcf().set_size_inches(w=9, h=6)
+            plt.savefig(Path(temp_dir, "waterfall_plot_html.png"), bbox_inches="tight")
+            waterfall_plot_b64 = b64_image(Path(temp_dir, "waterfall_plot_html.png"))
+            return waterfall_plot_b64
 
+def b64_image(image_filepath: Path) -> str:
+    with open(str(image_filepath), 'rb') as f:
+        image = f.read()
+    return 'data:image/png;base64,' + base64.b64encode(image).decode('utf-8')
 
-@app.callback(
-    Output(component_id="display_seuil", component_property="children"),
-    Input(component_id="submit-customer-id", component_property="n_clicks")
-)
-def display_seuil_classif(n_clicks):
-    r = requests.get(f'http://127.0.0.1:5000/api/model/params').json()
-    return f"Seuil de classification : {r['seuil_classif']}"
-
-# @app.callback(
-#     Output(component_id="shap-graph", component_property="figure"),
-#     Input(component_id='submit-customer-id', component_property='n_clicks'),
-#     State(component_id='customer_id', component_property='value')
-# )
-# def display_interpetabilty_plot(n_clicks, customer_id):
-#     if n_clicks > 0:
-#         r = requests.get(f'http://127.0.0.1:5000/api/customers/interpretability?id={customer_id}').json()
-#         shap_values = pd.DataFrame(data=r).values
-#         print(shap_values)
-#         params = requests.get(f'http://127.0.0.1:5000/api/model/params').json()
-#         explanation = shap.Explanation(values = shap_values, base_values=params["expected_value"], feature_names=params["features"])
-#         return None
-#         # return shap.plots.waterfall(explanation, matplotlib=True)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
