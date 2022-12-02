60fac8e0fe7f3c1f73623454a75fe37a25c19146
Hugo Pantecouteau <h.pantecouteau@beeguard.fr>
2022-12-02 10:40:20 +0100

    Tracer graphique bivarié

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,6 +98,47 @@ def get_all_train_data(data_dir: str) -> Tuple[pd.Series, pd.DataFrame]:
     return pd.read_csv(f"{data_dir}/df_train.csv") 
 
 
+@st.experimental_memo
+def draw_bivariate_plot(data: pd.DataFrame, x_var: str, y_var: str, customer_id: int):
+    assert isinstance(data, pd.DataFrame) and "SK_ID_CURR" in data.columns
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+    ax.scatter(
+        data.loc[:, x_var].values,
+        data.loc[:, y_var].values,
+        marker='+',
+        s=5,
+        alpha=0.25,
+        color='forestgreen'
+        )
+    ax.scatter(
+        data.loc[data.SK_ID_CURR == customer_id, x_var].values,
+        data.loc[data.SK_ID_CURR == customer_id, y_var].values,
+        marker='+',
+        s=10,
+        alpha=1,
+        color="firebrick"
+    )
+    ax.hlines(
+        y=data.loc[data.SK_ID_CURR == customer_id, y_var].values,
+        xmin=data.loc[:, x_var].min(),
+        xmax=data.loc[data.SK_ID_CURR == customer_id, x_var].values,
+        linestyle='--',
+        color="firebrick",
+        alpha=0.8
+    )
+    ax.vlines(
+        x=data.loc[data.SK_ID_CURR == customer_id, x_var].values,
+        ymin=data.loc[:, y_var].min(),
+        ymax=data.loc[data.SK_ID_CURR == customer_id, y_var].values,
+        linestyle='--',
+        color="firebrick",
+        alpha=0.8
+    )
+    ax.set(xlabel=x_var, ylabel=y_var)
+    ax.margins(0)        
+    return fig
+
+
     
 
 @st.experimental_memo
@@ -125,6 +166,13 @@ if "df_customer_data" in st.session_state:
     st.dataframe(show_filtered_dataframe(st.session_state.df_customer_data, additional_var_to_show))
     numerical_vars = [var for var in df_customers.select_dtypes('number').columns if var != "SK_ID_CURR" and "FLAG" not in var]
     col_left, col_right = st.columns(2)
+    with col_right:
+        st.markdown("### Situation du client dans un nuage de points :")
+        st.selectbox("Axe des abscisses (en bas) : ", options=numerical_vars, key="x_var", index=0)
+        st.selectbox("Axe des ordonnées (à gauche) : ", options=numerical_vars, key="y_var", index=1)
+        bivariate_plot = draw_bivariate_plot(df_customers, st.session_state.x_var, st.session_state.y_var, st.session_state.customer_id)
+        st.pyplot(bivariate_plot)
+
 # shap_values = pd.DataFrame(st.session_state.r_shap_customer).values[:,0]
 params = requests.get(f'http://127.0.0.1:5000/api/model/params').json()
 df_shap_customer = build_df_shap_customer(st.session_state.customer_id)
