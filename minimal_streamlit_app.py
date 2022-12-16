0944b19f0acd755172e32c34c9dc97a798c749fb
Hugo Pantecouteau <h.pantecouteau@beeguard.fr>
2022-12-16 12:27:00 +0100

    Clean not needed code for minimal app (test)

--- minimal_streamlit_app.py
+++ minimal_streamlit_app.py
@@ -1,13 +1,5 @@
-import base64
-from pathlib import Path
 from typing import List, Tuple
-import plotly.graph_objects as go
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
 import pandas as pd
-import requests
-import shap
 import streamlit as st
 from gsheetsdb import connect
 
@@ -19,7 +11,6 @@ def run_query(query):
     return rows
 
 
-sns.set_theme()
 st.set_page_config(
     page_title="Tableau de bord - Crédit",
     layout="wide",
@@ -30,75 +21,11 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 conn = connect()
 sheet_url = st.secrets["public_gsheets_url"]
-df_train = run_query(f'SELECT * FROM "{sheet_url}"')
+X_train = run_query(f'SELECT * FROM "{sheet_url}"')
+df_train = pd.DataFrame(X_train)
 
-
-def b64_image(image_filepath: Path) -> str:
-    with open(str(image_filepath), 'rb') as f:
-        image = f.read()
-    return 'data:image/png;base64,' + base64.b64encode(image).decode('utf-8')
-
-
-def build_data_df(df_stats: pd.DataFrame):
-    r = get_customer_info(st.session_state.customer_id)
-    df = pd.DataFrame.from_dict(data=r, orient="index")
-    df.columns=["Client"]
-    st.session_state.df_customer_data = (
-        pd.merge(df, df_stats, how="left", left_index=True, right_index=True)
-        .rename(columns={
-            "0":"Client",
-            "mean": "Moyenne des clients",
-            "std": "Dispersion",
-            "50%": "Médiane des clients",
-            "min": "Minimum",
-            "max": "Maximum",})
-        .drop(columns=["count", "25%", "75%"], index=["SK_ID_CURR", "TARGET"])
-    )
-
-
-@st.experimental_memo
-def build_df_shap_customer(customer_id: int):
-    r = get_customer_shap(customer_id)
-    return pd.DataFrame.from_dict(data=r, orient="index")
-
-
-@st.experimental_memo
-def get_customer_info(customer_id: int):
-    r = requests.get(f'http://127.0.0.1:5000/api/customers?id={customer_id}').json()
-    return r
-    
-
-@st.experimental_memo
-def get_customer_proba(customer_id: int):
-    r = requests.get(f'http://127.0.0.1:5000/api/customers/proba?id={customer_id}').json()
-    return r
-
-
-@st.experimental_memo
-def get_customer_shap(customer_id: int):
-    r = requests.get(f'http://127.0.0.1:5000/api/customers/interpretability?id={customer_id}').json()
-    return r
-
-
-@st.experimental_memo
-def decision_attribution(proba):
-    if isinstance(proba, float):
-        if proba >= st.session_state.r_params["seuil_classif"]:
-            return "Accordé"
-        else:
-            return "Refusé"
-    else:
-        return np.nan
-
-
-@st.experimental_memo
-def get_all_train_data(data_dir: str) -> pd.DataFrame:
-    return pd.read_csv(f"{data_dir}/df_train.csv") 
-
-
-with st.spinner("Chargement..."):
-    st.session_state.r_params = requests.get(f'http://127.0.0.1:5000/api/model/params').json()
-    st.write(df_train[1])
+st.write(X_train[1])
+st.dataframe(df_train.head())
 
 
 
