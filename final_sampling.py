8fe70d14153c2bb9da5c623094190598da8039b0
Hugo Pantecouteau <h.pantecouteau@beeguard.fr>
2023-01-19 13:55:09 +0100

    Resample

--- final_sampling.py
+++ final_sampling.py
@@ -4,7 +4,7 @@ df_input = pd.read_csv("input.csv")
 df_shap = pd.read_csv("df_shap.csv")
 print(f"df_input : {df_input.shape}")
 print(f"df_shap : {df_shap.shape}")
-sampled_df_shap = df_shap.sample(frac=0.1, random_state=0)
+sampled_df_shap = df_shap.sample(n=500, random_state=0)
 sampled_df_input = df_input.loc[df_input.SK_ID_CURR.isin(sampled_df_shap.SK_ID_CURR.values),:]
 print(f"sampled_df_shap : {sampled_df_shap.shape}")
 print(f"sampled_df_input : {sampled_df_input.shape}")
